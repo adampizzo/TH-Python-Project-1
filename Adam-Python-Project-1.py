a74225febffb818737094940128425ddfcdcb160
adampizzo <Adampizzo@icloud.com>
2022-02-12 12:18:45 -0500

    Update Adam-Python-Project-1.py
    - Rewrote Entire Logic system behind how it is guessing the number. Instead of just throwing the numbers out after each guess, it will append them to a list that I am then returning for other functions to append to a list in start_game() that I can keep for the future.
    - Utilizing len() of the list that is output to determine how many guesses it took for both the score and high score.
    
    -Fixed the get_guess() function so that it doesn't crash on initial error (number not being an int). Couldn't figure out how I wanted to do it so it would show the bad input in the error and keep the input wrapped by int(). So I've added the guess = int(guess)  line instead. Not ideal, but I don't have an answer yet. I couldn't catch the UnboundLocalError at all to do anything with it.
    - added return values for start_game() if I ever wanted to add more to outside the function. (Maybe terminal with multiple games and menus and I can set those fields for calls on high score or last round or something.)

--- Adam-Python-Project-1.py
+++ Adam-Python-Project-1.py
@@ -17,76 +17,97 @@ class GuessTooLow(Exception):
     pass
 class GuessTooHigh(Exception):
     pass
+class AlreadyGuessed(Exception):
+    pass
 
 
 
 def sleep():
-    time.sleep(.75)
+    time.sleep(.5)
 
 
 def print_score(score):           
     if score == 1:
-        print("You guessed it in only {} guess, no beating that!\n".format(score))
+        print(f"You guessed it in only {score} guess, no beating that!\n")
         sleep()
     else:
-        print("You guessed it in {} guesses!\n".format(score))
+        print(f"You guessed it in {score} guesses!\n")
         sleep()
 
 
 def evaluate_high_score(score, high_score):
     if high_score == 9999:
-        print("You have set the high score at: {}\nCongratulations!\n".format(score))
+        print(f"You have set the high score at: {score}\nCongratulations!\n")
         return score
     else: 
-        print("The previous high score was: {}...".format(high_score))
+        print(f"The previous high score was: {high_score}...")
         if score < high_score:
-            print("But it looks like you just beat it with your score of: {}\nGreat Job!\n".format(score))
+            print(f"But it looks like you just beat it with your score of: {score}\nGreat Job!\n")
             sleep()
             return score
         elif score == high_score:
-            print("It looks like you tied the high score at: {}.\nSo close!\n".format(high_score))
+            print(f"It looks like you tied the high score at: {high_score}.\nSo close!\n")
             sleep()
             return high_score
         else:
             print("And the record is still intact!\nBetter luck next time!\n")
             sleep()
-            return high_score   
+            return high_score  
 
 
-def evaluate_guess(answer):    
-    wrong_guess_counter = 1
-    while True:        
+def get_guess():    
+    while True:                
         try:
             guess = input("Guess a number between 1 and 10:\n")
-            guess = int(guess)
-            if type(guess) != int:
-                raise ValueError
-            elif guess < 1 or guess > 10:
-                raise GuessOutOfRangeError("\n{} is not between 1 and 10. Try Again.\n".format(guess))
-            elif guess > answer:
-                raise GuessTooHigh("\nThe secret number is lower.\n")
-            elif guess < answer:
-                raise GuessTooLow("\nThe secret number is higher.\n")
-            break
-        except ValueError:
-            print("\n{} not a valid number, please try again.\n".format(guess))            
+            guess = int(guess) #I don't like doing it this way, but I haven't found an answer yet                             
+            if guess < 1 or guess > 10:
+                raise GuessOutOfRangeError(f"\n{guess} is not between 1 and 10. Try Again.\n")
+        except UnboundLocalError:
+            print("Define Guess First")
+        except ValueError:            
+            print(f"\n{guess} not a valid number, please try again.\n")
         except GuessOutOfRangeError as err:
             print(err)
+        else:
+            return guess
+
+
+def guess_in_history(history):
+    while True:        
+        guess = get_guess()        
+        try:
+            if guess in history:
+                raise AlreadyGuessed(f"{guess} has already been guessed, please try again")
+            else:
+                history.append(guess)
+                break
+        except AlreadyGuessed as err:
+            print(err)
+    return guess, history
+
+
+def evaluate_guess(answer):        
+    guesses = []
+    while True: 
+        try:
+            guess, guesses = guess_in_history(guesses)
+            if guess > answer:                
+                raise GuessTooHigh("\nThe secret number is lower.\n")
+            elif guess < answer:                
+                raise GuessTooLow("\nThe secret number is higher.\n")                
+            break        
         except GuessTooHigh as err:
-            wrong_guess_counter += 1
             print(err)
         except GuessTooLow as err:
-            wrong_guess_counter += 1
-            print(err)
-
+            print(err)            
     print("\nYou Got it!\n")
     sleep()        
-    print_score(wrong_guess_counter)
+    print_score(len(guesses))
     sleep()
-    return wrong_guess_counter
+    return len(guesses), guesses
 
 
-def continue_game(num_rounds):
+def continue_game(num_rounds, round_history):
     while True:        
         try:
             question = input("Do you want to play the game again? (Y/N)\n")
@@ -102,11 +123,11 @@ def continue_game(num_rounds):
                 return True
             elif question.lower() == "n":
                 if num_rounds == 1:
-                    print("You played for {} round.\n".format(num_rounds))
+                    print(f"You played for {num_rounds} round.\n")
                 else:
-                    print("You played for {} rounds.\n".format(num_rounds))
+                    print(f"You played for {num_rounds} rounds.\n")
                 sleep()
-                print("Game ending. Thank you for playing.")
+                print("Game ending. Thank you for playing.")                
                 sleep()
                 return False
                 
@@ -114,22 +135,25 @@ def continue_game(num_rounds):
 def start_game():
     keep_playing = True
     high_score = 9999
-    rounds = 1    
+    player_score = 0
+    round_history = [] #Not really using this yet, but it is keeping track of all of the player guesses.       
     while keep_playing:       
         print(("="*40) , "> Welcome To The Number Guessing Game <" , ("="*40))
-        if rounds == 1:
+        if player_score == 0:
             print("\nThe objective of this game is to guess a random number between 1 and 10.")
             sleep()
             print("When you finally guess the number correctly we will let you know how many guesses it took.\n")
             sleep()        
         solution = random.randint(1,10)               
         if high_score != 9999:
-            print("The high score is currently: {}.\n".format(high_score))
+            print(f"The high score is currently: {high_score}.\n")
             sleep()   
-        player_score = evaluate_guess(solution)
+        player_score, guess_history = evaluate_guess(solution)
+        round_history.append(guess_history)
         high_score = evaluate_high_score(player_score, high_score)
-        keep_playing = continue_game(rounds)
-        rounds += 1
+        keep_playing = continue_game(player_score, round_history)
+    #return round_history, high_score Could use this to output the high score and player results for use in another function.
+        
 
 
 start_game()
